fa2c5c04f097acf3a7d63b9500b76e228a0da587
Ditam Gupta <ditamrauniyar@gmail.com>
2025-07-16 17:38:52 +0545

    Learned Data Types and conversion

---
+++ day_2/script.js
@@ -0,0 +1,28 @@
+let score = 33;
+console.log(typeof score);
+
+score = "33";
+console.log(typeof score);
+const scoreInNumber = Number(score);
+console.log(typeof scoreInNumber);
+
+let bool = true;
+console.log(typeof bool);
+let boolInNumber = Number(bool);
+console.log(typeof boolInNumber);
+console.log(boolInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let abc = "33abc";
+console.log(typeof abc);
+let abcToNum = Number(abc);
+console.log(typeof abcToNum);
+console.log(abcToNum);
+
+let someNumber = 33;
+let srtingNumber = String(someNumber);
+console.log(typeof srtingNumber);
