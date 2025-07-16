5b6659c2249b9e1f0d493eec93f2eba66c61adb2
Ditam Gupta <ditamrauniyar@gmail.com>
2025-07-16 21:24:22 +0545

    Learnt Numbers and Maths In JavaScript

---
+++ day_5/script.js
@@ -0,0 +1,33 @@
+const score = 400;
+// console.log(score);
+
+const newNum = new Number("Hello");
+// console.log(newNum);
+
+const balance = new Number(1000.566);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1023.8966;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+//------------MATHS----------------
+
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 6, 1, 8, 9));
+// console.log(Math.max(4, 5, 16, 1, 8, 9));
+
+const max = 20;
+const min = 10;
+// console.log(Math.random() * 10) + 1;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+console.log(Math.floor(Math.random() * 11) + min);
