a3bddece599850e208c6e3fe24900f56914b6cbc
Ditam Gupta <ditamrauniyar@gmail.com>
2025-07-16 20:43:32 +0545

    Learnt strings and their methods

---
+++ day_4/script.js
@@ -0,0 +1,25 @@
+const name = "Ditam";
+const repoCount = 100;
+// console.log(`My name is ${name} and I have ${repoCount} repos`);
+// console.log(name[0]);
+
+const gameName = new String("hitesh-hc");
+// console.log(gameName[0]);
+// console.log(gameName);
+// console.log(String(gameName));
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("t"));
+
+// console.log(gameName.substring(0, 4));
+// console.log(String(gameName).toUpperCase());
+// console.log(String(gameName).substring(0, 5).toUpperCase());
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+console.log(String(gameName).replace("e", "cd"));
+
+console.log(gameName.toString().replace("s", "hhh"));
