842fa76dfa576dca4aafbcc3afe19347dd744020
Ditam Gupta <ditamrauniyar@gmail.com>
2025-07-16 14:02:51 +0545

    Started my jouney with JavaScript

--- day_1/script.js
+++ day_1/script.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+/* Prefer nont to use var
+because it has function scope and can lead to unexpected behavior.
+*/
+const accountId = 144553;
+let accountEmail = "abc@gmail.com";
+var accountpassword = "12345";
+accountCity = "New York";
+
+// accountId = 2; not alloed as const variables cannot be reassigned
+
+// console.log(accountId);
+
+accountEmail = "aa@gmail.com";
+accountpassword = "45454545";
+accountCity = "Los Angeles";
+
+console.log([accountId, accountEmail, accountpassword, accountCity]);
