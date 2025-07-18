453690b3e46baa4c7d8ae47f318425d1344a9609
Ditam Gupta <ditamrauniyar@gmail.com>
2025-07-18 15:25:25 +0545

    Modified some changes here

--- day_3/script.js
+++ day_3/script.js
@@ -7,21 +7,6 @@ let str2 = " World";
 let str3 = str1 + str2;
 console.log(str3);
 
-let x = 3;
-const y = x++;
-console.log(y); // Outputs: 3, because y is assigned the value of x before
-console.log(x); // Outputs: 4, because x is incremented after the assignment
-console.log(typeof x);
-
-let value = 2;
-let negValue = -value;
-
-let str1 = "Hello";
-let str2 = " World";
-
-let str3 = str1 + str2;
-console.log(str3);
-
 let x = 3n;
 const y = x++;
 console.log(y); // Outputs: 3, because y is assigned the value of x before
